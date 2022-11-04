254fd40179accb6e716d94abb944bf26334f7af2
Anup Jaiswal <94697354+janup9967@users.noreply.github.com>
2022-11-04 23:35:30 +0530

    Rename EXP2.asm to EXP_2.asm

--- EXP_2.asm
+++ EXP_2.asm
@@ -1,7 +1,5 @@
 
-; Apply Assembly Language Programing to perform  addition and subtraction of two 16 bits numbers using macros and procedure.
-
-
+;Apply Assembly Language Programing to covert HEX to BCD.
 
 
 .model small
